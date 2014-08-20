4c122f89b0e4538938d646afcfadf5f6115a245b
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2014-08-19 23:50:34 -0400

    Externalized default enumeration initializations to appropriate services.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,6 +10,8 @@ import org.commonsemantics.grails.users.utils.DefaultUsersRoles
 class BootStrap {
 
 	def grailsApplication
+	def usersRolesService
+	def usersProfilePrivacyService
 	
     def init = { servletContext ->
 		
@@ -33,23 +35,26 @@ class BootStrap {
 		log.info  '>> INITIALIZING DEFAULTS'
 		separator();
 		log.info  '** Users Roles'
+		usersRolesService.register();
+		log.info  '** Users Profile Privacy'
+		usersProfilePrivacyService.register();
 		
-		DefaultUsersRoles.values().each {
-			log.info  '** ' + it.value()
-			if(!Role.findByAuthority(it.value())) {
-				new Role(authority: it.value(), ranking: it.ranking(), label: it.label(), description: it.description()).save(failOnError: true)
-				log.info "Initialized: " + it.value()
-			}
-		}
+//		DefaultUsersRoles.values().each {
+//			log.info  '** ' + it.value()
+//			if(!Role.findByAuthority(it.value())) {
+//				new Role(authority: it.value(), ranking: it.ranking(), label: it.label(), description: it.description()).save(failOnError: true)
+//				log.info "Initialized: " + it.value()
+//			}
+//		}
 		
-		DefaultUsersProfilePrivacy.values().each {
-			if(!ProfilePrivacy.findByValue(it.value())) {
-				new ProfilePrivacy(value: it.value(), label: it.label(), description: it.description()).save(failOnError: true)
-				log.info "Initialized: " + it.value()
-			} else {
-				log.info "Found: " + it.value()
-			}
-		}
+//		DefaultUsersProfilePrivacy.values().each {
+//			if(!ProfilePrivacy.findByValue(it.value())) {
+//				new ProfilePrivacy(value: it.value(), label: it.label(), description: it.description()).save(failOnError: true)
+//				log.info "Initialized: " + it.value()
+//			} else {
+//				log.info "Found: " + it.value()
+//			}
+//		}
 		
 		separator();
 		log.info  '>> USERS'
@@ -74,6 +79,7 @@ class BootStrap {
 		if(admin==null) {
 			admin = new User(username: adminUsername,
 				password: password, person: person, firstName: 'Jack', lastName: 'White',
+				profilePrivacy:  ProfilePrivacy.findByValue(DefaultUsersProfilePrivacy.PRIVATE.value()),
 				displayName: 'Dr. White', enabled: true, email:'paolo.ciccarese@gmail.com').save(failOnError: true)
 			log.warn  "CHANGE PASSWORD for: " + adminUsername + "!!!"
 		}
